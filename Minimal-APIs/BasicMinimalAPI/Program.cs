2b0303d54b300b42327f2c8a61daf2c3c97f0b8f
Maninder Singh <maninder123450@gmail.com>
2025-03-31 12:12:59 +0100

    Added staticFile middleware and logging

--- Minimal-APIs/BasicMinimalAPI/Program.cs
+++ Minimal-APIs/BasicMinimalAPI/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddHttpLogging(configureOptions => 
+    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.Response);
+
+// Ensures that logs added by the HTTP logging middleware are visible in the log output
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
+
+WebApplication? app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpLogging(); // HTTP logging middleware logs each request to the application in the log output.
+}
+
+//app.UseWelcomePage();
+app.UseStaticFiles();   
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/person", () => new Person("Adam", "smith"));   
 
 app.Run();
+
+public record Person(string FirstName, string LastName); 
+
