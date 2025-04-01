f440b502b8d035f027a633cca78e2662f17f6002
Maninder Singh <maninder123450@gmail.com>
2025-04-01 10:17:49 +0100

    Added exeption handler middleware and added production profile

--- Minimal-APIs/BasicMinimalAPI/Program.cs
+++ Minimal-APIs/BasicMinimalAPI/Program.cs
@@ -1,29 +1,50 @@
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.Extensions.Logging.Abstractions;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddHttpLogging(configureOptions => 
-    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.Response);
+    configureOptions.LoggingFields = HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders);
 
 // Ensures that logs added by the HTTP logging middleware are visible in the log output
 builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
 
 WebApplication? app = builder.Build();
 
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error");
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseHttpLogging(); // HTTP logging middleware logs each request to the application in the log output.
+    app.UseDeveloperExceptionPage(); // only use in development 
 }
 
 //app.UseWelcomePage();
+
 app.UseStaticFiles();   
+app.UseRouting();
+
+// Error handling 
+app.MapGet("/error", () => "Sorry, There was an problem processing your request.");
+app.MapGet("testerror", () => GenerateError.ThrowError()); // Testing exeption handling middleware 
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Adam", "smith"));   
+app.MapGet("/person", () => new Person("Adam", "smith"));
+
+
 
 app.Run();
 
-public record Person(string FirstName, string LastName); 
+record Person(string FirstName, string LastName);
 
+internal class GenerateError
+{
+    public static string ThrowError() => throw new Exception("Intentional Error"); 
+}
