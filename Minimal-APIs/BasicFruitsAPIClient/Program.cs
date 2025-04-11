0bcdbc87794df1aef961806d113aef7257ed265b
Maninder Singh <maninder123450@gmail.com>
2025-04-11 12:10:15 +0100

    Console application to test api

---
+++ Minimal-APIs/BasicFruitsAPIClient/Program.cs
@@ -0,0 +1,15 @@
+﻿//using Fruit;
+
+//IFruitClient client = new FruitClient(
+//    new HttpClient() { BaseAddress = new Uri("https://localhost:7000") });
+
+
+//var created = client.FruitAsync(new Fruit.Fruit { Name = "Banana1000" });
+//Console.WriteLine($"Created {created.Result.Name}");
+
+//created.Wait();
+
+//var fetched = client.NameAsync(created.Result.Name);
+//Console.WriteLine($"Fetched {fetched.Result.Name}");
+
+Console.WriteLine("hello");
