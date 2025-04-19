481ad826a44436f647e697ce5c7685884c979ce0
Maninder Singh <maninder123450@gmail.com>
2025-04-19 09:19:48 +0100

    Created extention to add databse service

---
+++ Minimal-APIs/SchoolAPI/SchoolAPI/Data/DatabaseServiceCollectionExtension.cs
@@ -0,0 +1,24 @@
+﻿using DatabaseContext;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolAPI.Data; 
+
+public static class DatabaseServiceCollectionExtension
+{
+    public static IServiceCollection AddSchoolDatabaseService(this IServiceCollection services)
+    {
+        services.AddDbContext<SchoolForApiContext>(options
+            => options.UseSqlServer(new SqlConnectionStringBuilder()
+            {
+                DataSource = ".\\LEARNING",
+                InitialCatalog = "SchoolForAPI",
+                IntegratedSecurity = true,
+                ConnectTimeout = 5,
+                Encrypt = true,
+                TrustServerCertificate = true
+            }.ConnectionString));
+
+        return services; 
+    }
+}
