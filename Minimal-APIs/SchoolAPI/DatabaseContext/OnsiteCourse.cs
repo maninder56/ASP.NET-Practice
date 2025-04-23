e4efae436856e8d5fa964ce177be4005da7d265d
Maninder Singh <maninder123450@gmail.com>
2025-04-23 10:03:51 +0100

    Added data annotations

--- Minimal-APIs/SchoolAPI/DatabaseContext/OnsiteCourse.cs
+++ Minimal-APIs/SchoolAPI/DatabaseContext/OnsiteCourse.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DatabaseContext;
 
 public partial class OnsiteCourse
 {
+    [Required]
     public int CourseId { get; set; }
 
+    [Required]
+    [StringLength(50)]
     public string Location { get; set; } = null!;
 
+    [Required]
+    [StringLength(50)]
     public string Days { get; set; } = null!;
 
+    [Required]
     public DateTime Time { get; set; }
 
     public virtual Course Course { get; set; } = null!;
