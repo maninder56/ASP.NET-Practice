760988317e187fdabf24abb18508ac29cbea17c3
Maninder Singh <maninder123450@gmail.com>
2025-04-28 12:02:25 +0100

    Interface for database service

---
+++ Minimal-APIs/SchoolAPI/SchoolAPI/Services/IInstructorDatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using DatabaseContext;
+
+namespace SchoolAPI.Services;
+
+public interface IInstructorDatabaseService
+{
+    // Helper Methods 
+    public bool InstructorExists(int instructorId);
+
+    // Read Operations 
+    public List<Person> GetAllInstructors();
+    public Person? GetInstructorByID(int instructorID); 
+
+    // Create Operations 
+    public Person? CreateInstructor(Person instructor);
+
+    // Update Operations 
+    public Person? UpdateInstructorByID(int instructorID, Person instructor);
+
+    // Delete Operations 
+    public Person? DeleteInstructorByID(int instructorID);
+}
