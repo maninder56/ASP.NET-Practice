35f2c3db48d7c34cbb67ccf492668272736007c3
Maninder Singh <maninder123450@gmail.com>
2025-05-07 12:04:43 +0100

    added new project

---
+++ WebAppWithRazorPages/ShoppingList/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShoppingList.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
