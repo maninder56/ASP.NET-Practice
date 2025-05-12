99dc9dfc4bca38f13d32080c443d3b01e8a9fd61
Maninder Singh <maninder123450@gmail.com>
2025-05-12 09:24:53 +0100

    Inital application

---
+++ SecuringAndDeployment/BasicRazorAppWithIdentity/DataBaseScripts/localDB.sql
@@ -0,0 +1,45 @@
+SELECT name
+FROM master.sys.databases; 
+GO
+
+--USE dummy; 
+--GO
+
+
+--USE [aspnet-BasicRazorAppWithIdentity-cfa044bf-c5c7-4b41-8bda-e0a529e73572]; 
+--GO 
+
+
+SELECT T.TABLE_NAME, T.TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES T; 
+
+
+
+SELECT *
+FROM AspNetRoles; 
+
+SELECT *
+FROM AspNetRoleClaims;
+
+SELECT *
+FROM AspNetUserRoles; 
+
+
+SELECT *
+FROM AspNetUsers; 
+
+SELECT *
+FROM AspNetUserLogins; 
+
+SELECT *
+FROM AspNetUserClaims; 
+
+SELECT *
+FROM AspNetUserTokens; 
+
+
+
+
+
+
+
